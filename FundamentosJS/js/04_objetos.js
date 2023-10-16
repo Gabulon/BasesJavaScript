75fbcdb5e41c026df7d924c64573ae6a52eabb07
Gabulon <gabo.fajardo7@gmail.com>
2023-10-15 20:40:55 -0600

    objetos

---
+++ FundamentosJS/js/04_objetos.js
@@ -0,0 +1,20 @@
+//objetos
+// const producto={
+//     nombre:"tablet",
+//     precio: 200,
+//     disponible: true
+// }
+// console.log(producto)
+
+//Destructuring
+// const{nombre,precio,dispnoble}=producto
+// console.log(nombre)
+
+//object literal enhacement 
+const autenticado= true
+const usuario="Gabriel"
+const nuevoObjeto={
+   autenticado,
+    usuario
+}
+console.log(nuevoObjeto)
