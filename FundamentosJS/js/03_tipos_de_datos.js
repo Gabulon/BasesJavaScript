ceeadd6ec91e313198fccda1bf96a896f65d421f
Gabulon <gabo.fajardo7@gmail.com>
2023-10-15 20:39:58 -0600

    tipos de datos

---
+++ FundamentosJS/js/03_tipos_de_datos.js
@@ -0,0 +1,35 @@
+//Tipos de datos
+
+//undefided
+// let cliente
+// console.log(typeof cliente)
+
+//Bolean
+//  const descuento=false
+//  console.log(typeof descuento)
+
+
+//Number
+// const numero1=20.20
+// const numero2=30
+// const numero3=-100
+// console.log(typeof numero1)
+
+//strings o cadenas de texto
+// const alumno="Gabriel"
+// let producto='Monitor de 20 pulgadas'
+// console.log(typeof producto)
+
+//BigInt
+// const numeroGrande=BigInt(184347017407312730912392012301263064)
+// console.log(typeof numeroGrande)
+
+//symbol
+// const primerSymbol=Symbol(30)
+// const segundoSymbol=Symbol(30)
+// console.log(primerSymbol=== segundoSymbol)
+
+//Null
+const descuento=null
+console.log(typeof precioDescuento)
+
