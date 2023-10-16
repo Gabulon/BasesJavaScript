fff5d45c69dd8f267523f9723c6ac252ca29ebda
Gabulon <gabo.fajardo7@gmail.com>
2023-10-15 20:35:53 -0600

    variables let

---
+++ FundamentosJS/js/01_variables.js
@@ -0,0 +1,13 @@
+//variables var let const
+
+console.log('hola mundo');
+
+let cliente ="Gabriel"
+
+let precioDescuento=220
+
+//let se pueden reasignar y iniciar sin valor
+
+console.log(cliente);
+console.log(precioDescuento);
+
