984f3d65d265d3b057ac383971931f79de5afc43
Gabulon <gabo.fajardo7@gmail.com>
2023-10-15 20:37:57 -0600

    variables const

---
+++ FundamentosJS/js/02_variablesConst.js
@@ -0,0 +1,11 @@
+//variables-const
+const cliente ="Gabriel"
+
+const precioDescuento=220
+
+//No se peude reasignar y no pueden iniciar sin valor
+//No pueden iniciar con numeros o caracteres especiales (guion bajo si esta permitido)
+//  las variables no pueden tener espacios 
+// se recomienda el uso del camel case
+console.log(cliente)
+console.log(precioDescuento)
