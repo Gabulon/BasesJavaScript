6dcf1e4b0cb9da5fd923e2b85537560468cb5f96
Gabulon <gabo.fajardo7@gmail.com>
2023-10-22 17:41:52 -0600

    Iteracion de arreglos

---
+++ FundamentosJS/js/11_iteradores_arreglos.js
@@ -0,0 +1,15 @@
+//iteradores en js
+const tecnologias =['HTML','CSS','JavaScript','React','Node.js']
+
+//forEach - Accedes a cada elemento del array
+const arrayforeach = tecnologias.forEach(function (tech){
+    return tech
+})
+
+//Crear un nuevo array
+const arraymap = tecnologias.map(function (tech){
+    return tech
+})
+
+console.log(arrayforeach)
+console.log(arraymap)
