635feaed6d01cb0195789d179fb6942c91d0d86a
Gabulon <gabo.fajardo7@gmail.com>
2023-10-22 17:57:23 -0600

    Funciones que retornan valores

--- FundamentosJS/js/12_funcionesyDeclaraciones.js
+++ FundamentosJS/js/12_funcionesyDeclaraciones.js
@@ -1,10 +1,22 @@
 //funciones y declaraciones
 
-function sumar(numero=0, numero2=0){
-   console.log(numero + numero2)
-}
+// function sumar(numero=0, numero2=0){
+//    console.log(numero + numero2)
+// }
 
-sumar(20,30)
-sumar(2,3)
-sumar(100)
-sumar()
+// sumar(20,30)
+// sumar(2,3)
+// sumar(100)
+// sumar()
+
+function sumar(numero=0, numero2=0){
+    return {
+        resultado: numero + numero2,
+        mesnaje: 'Hola Mundo'
+    }
+ }
+
+ const {resultado,mensaje}=sumar(20,30)
+
+ console.log(resultado)
+ console.log(mensaje)
