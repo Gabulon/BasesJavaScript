ccf70f819b36e28015b904e99907111498e98db6
Gabulon <gabo.fajardo7@gmail.com>
2023-10-22 17:51:13 -0600

    Funciones y parametros por default

--- FundamentosJS/js/12_funcionesyDeclaraciones.js
+++ FundamentosJS/js/12_funcionesyDeclaraciones.js
@@ -1,7 +1,10 @@
 //funciones y declaraciones
 
-function sumar(){
-    console.log(2+2)
+function sumar(numero=0, numero2=0){
+   console.log(numero + numero2)
 }
 
+sumar(20,30)
+sumar(2,3)
+sumar(100)
 sumar()
