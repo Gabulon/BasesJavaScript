e0dbb0e88ac8a535060f6810b9bf1e389892d7fe
Gabulon <gabo.fajardo7@gmail.com>
2023-10-22 18:14:55 -0600

    Funciones de flecha

---
+++ FundamentosJS/js/14_funciones_flecha.js
@@ -0,0 +1,10 @@
+// Funciones -Arrow functions
+
+const sumar = (numero, numero2) =>  numero + 20
+const sumarArrow= ()=> 2+2
+
+const resultado= sumar(30,30)
+const resultado2= sumarArrow()
+
+console.log(resultado)
+console.log(resultado2)
