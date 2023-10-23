d32565b016c0856a0b27a45b66d5038eafd88758
Gabulon <gabo.fajardo7@gmail.com>
2023-10-22 18:04:20 -0600

    Funciones de expresion

---
+++ FundamentosJS/js/13_funciones_expresion.js
@@ -0,0 +1,12 @@
+//Funciones - Function Expression
+
+function sumar(numero=0, numero2=0){
+   console.log(numero + numero2)
+}
+
+const sumar= function(numero = 0, numero2= 0){
+return numero + numero2
+}
+
+const resultado=sumar(30)
+console.log(resultado)
