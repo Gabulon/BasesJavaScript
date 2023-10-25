1339cb084d2376483366a126e99af728a37d64dc
Gabulon <gabo.fajardo7@gmail.com>
2023-10-25 16:58:29 -0600

    condicionales and y or

---
+++ FundamentosJS/js/19_condicionale_or_and.js
@@ -0,0 +1,12 @@
+const saldo =600
+const pagar =500
+const tarjet =true
+// || almenos una debe cumplirse
+// && - todas deben cumplirse
+
+if(saldo >pagar || tarjet){
+    console.log('puedes pagar')
+
+}else{
+    console.log('No, no puedes pagar')
+}
