4cb895a79ee1a5daeeda1877e7fd6846d7d7bad8
Gabulon <gabo.fajardo7@gmail.com>
2023-10-25 17:54:31 -0600

    Condicionales ternarios

---
+++ FundamentosJS/js/20_condicionales_ternarios.js
@@ -0,0 +1,17 @@
+const autenticado =true
+
+autenticado ?
+ console.log('usuario autenticado'):
+ console.log('No autenticado,dirigir hacia login')
+
+ //doble Ternario
+
+ const saldo =600
+ const pagar =800
+ const tarjeta= false
+
+ saldo > pagar ?
+ console.log('Puede pagar con saldo'):
+ tarjeta?
+ console.log('Puedes pagar con tarjeta'):
+ console.log('No no puedes pagar')
