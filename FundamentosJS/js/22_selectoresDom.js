6f6af8878c40e2936a2f8d6440592105a51a28b5
Gabulon <gabo.fajardo7@gmail.com>
2023-10-25 19:29:03 -0600

    Selectoresdel Dom

---
+++ FundamentosJS/js/22_selectoresDom.js
@@ -0,0 +1,14 @@
+// Selectores DOM
+
+const heading = document.querySelector('.heading')
+
+console.log(heading)
+console.log( heading.textContent)
+console.log(heading.tagName)
+console.log(heading.classList)
+console.log(heading.tagName)
+console.log(heading.id)
+
+const enlaces =document.querySelector('.navegacion a')
+
+console.log(enlaces)
