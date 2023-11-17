e6d9121c73a361cfdb290662ba8e3dd8590c37b8
Gabulon <gabo.fajardo7@gmail.com>
2023-11-17 16:00:31 -0600

    Manipular HTML con Js

---
+++ FundamentosJS/js/23_manipular_HTML_heading.js
@@ -0,0 +1,13 @@
+//manipular elementos de HTML con JS
+
+const heading = document.querySelector('.heading')
+heading.textContent = 'Un nuevo Heading'
+
+console.log(heading.textContent)
+
+const imputNombre = document.querySelector('#nombre')
+inputNombre.value = 'Un valor por default'
+
+const enlaces = document.querySelectorAll('.navegacion a')
+
+enlaces.forEach(enlace => enlace.textContent='Nuevo Enlace')
