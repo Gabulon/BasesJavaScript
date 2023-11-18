5a628ce9cc95871f8a341df444155aaec7e065b9
Gabulon <gabo.fajardo7@gmail.com>
2023-11-17 18:27:07 -0600

    multiples async await

---
+++ FundamentosJS/js/31_multiples async_await.js
@@ -0,0 +1,38 @@
+// fetch API - Async Await(Multiples llamados)
+
+const url = "https://jsonplaceholder.typicode.com/comments"
+const url2 = "https://jsonplaceholder.typicode.com/comments"
+
+const consultarAPI = async ()=>{
+    const inicio = performance.now()
+
+    const respuesta = await fetch(url)
+    const resultado = await respuesta.json()
+    console.log (resultado)
+
+    const respuesta2 = await fetch(url2)
+    const resultado2 = await respuesta2.json()
+    console.log (resultado2)
+
+    const fin = performance.now()
+
+    console.log(`Ejecución primer Async:${fin - inicio} ms `)
+}
+
+const consultarAPI2 = async ()=>{
+ const inicio = performance.now()
+ 
+ const [respuesta,respuesta2]=await Promise.all([fetch(url),fetch(url2)])
+ const resultado = await respuesta.json()
+ const resultado2 = await respuesta2.json()
+
+ console.log(resultado)
+ console.log(resultado2)
+
+ const fin = performance.now()
+
+ console.log(`Ejecución primer Async:${fin - inicio} ms `)
+}
+
+
+consultarAPI2();
